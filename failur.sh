b5cf28cffacdc474a6b4a37656c3711083b4bb5f
Coffee without sugar <88631273+Wonkiest29@users.noreply.github.com>
2024-01-05 12:19:16 +0100

    Update failur.sh

--- failur.sh
+++ failur.sh
@@ -38,3 +38,31 @@ apt install mysql-server
 # Install phpMyAdmin
 apt install phpmyadmin
 ln -s /usr/share/phpmyadmin /var/www/html
+
+#!/bin/bash
+
+# Fetch the latest version number from the phpMyAdmin GitHub releases page
+latest_version=$(curl -sL https://github.com/phpmyadmin/phpmyadmin/releases/latest | grep -oP 'tag/([0-9]+\.[0-9]+\.[0-9]+)' | grep -oP '([0-9]+\.[0-9]+\.[0-9]+)')
+
+# Check if the latest version number is retrieved successfully
+if [ -n "$latest_version" ]; then
+    echo "Latest phpMyAdmin version available: $latest_version"
+    
+    # Construct the download URL for the latest version
+    download_url="https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-$latest_version-all-languages.zip"
+
+    # Download the latest version
+    echo "Downloading phpMyAdmin $latest_version..."
+    wget "$download_url"
+    
+    # Unzip the downloaded file
+    unzip "phpMyAdmin-$latest_version-all-languages.zip"
+    mw phpMyAdmin-$latest_version-all-languages phpmyadmin
+    mw /usr/share/phpmyadmin /usr/share/phpmyadminold
+    mv phpmyadmin /usr/share/phpmyadmin
+    echo "Download and extraction complete."
+else
+    echo "Failed to retrieve the latest version of phpMyAdmin."
+fi
+
+
