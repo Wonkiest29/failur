cf7011dfd505b19145aa56a43f563bd372df639b
Coffee without sugar <88631273+Wonkiest29@users.noreply.github.com>
2024-01-05 02:04:08 +0100

    Update failur.sh

--- failur.sh
+++ failur.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
+# Update and upgrade packages
 apt-get update 
 apt-get upgrade 
 
+# Install Nginx and PHP-FPM
 apt install nginx 
 apt install php7.4-fpm
+
+# Remove default Nginx configuration
+rm /etc/nginx/sites-enabled/default 
+
+# Configure Nginx with PHP
+echo "server {
+    listen 80;
+    root /var/www/html;
+
+    index index.php;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}" > /etc/nginx/sites-enabled/default
+nginx -t
+# Install MySQL Server
 apt install mysql-server
 
+# Install phpMyAdmin
+apt install phpmyadmin
+ln -s /usr/share/phpmyadmin /var/www/html
