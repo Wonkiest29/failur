aa83c4629df6279aa96ec1194ff7879551144c63
Coffee without sugar <88631273+Wonkiest29@users.noreply.github.com>
2024-01-11 12:20:55 +0000

    Update failur.sh

--- failur.sh
+++ failur.sh
@@ -6,7 +6,7 @@ apt-get upgrade
 
 # Install Nginx and PHP-FPM
 apt install nginx 
-apt install php7.4-fpm
+apt install php8.1-fpm
 
 # Remove default Nginx configuration
 rm /etc/nginx/sites-enabled/default 
@@ -24,7 +24,7 @@ echo "server {
 
     location ~ \.php$ {
         include snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
     }
 
     location ~ /\.ht {
@@ -40,30 +40,30 @@ sudo mysql_secure_installation
 apt install phpmyadmin
 ln -s /usr/share/phpmyadmin /var/www/html
 
-#!/bin/bash
+# !/bin/bash
 
 # Fetch the latest version number from the phpMyAdmin GitHub releases page
-latest_version=$(curl -sL https://github.com/phpmyadmin/phpmyadmin/releases/latest | grep -oP 'tag/([0-9]+\.[0-9]+\.[0-9]+)' | grep -oP '([0-9]+\.[0-9]+\.[0-9]+)')
+#latest_version=$(curl -sL https://github.com/phpmyadmin/phpmyadmin/releases/latest | grep -oP 'tag/([0-9]+\.[0-9]+\.[0-9]+)' | grep -oP '([0-9]+\.[0-9]+\.[0-9]+)')
 
 # Check if the latest version number is retrieved successfully
-if [ -n "$latest_version" ]; then
-    echo "Latest phpMyAdmin version available: $latest_version"
+#if [ -n "$latest_version" ]; then
+   # echo "Latest phpMyAdmin version available: $latest_version"
     
     # Construct the download URL for the latest version
-    download_url="https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-$latest_version-all-languages.zip"
+   # download_url="https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-$latest_version-all-languages.zip"
 
     # Download the latest version
-    echo "Downloading phpMyAdmin $latest_version..."
-    wget "$download_url"
-    
-    # Unzip the downloaded file
-    unzip "phpMyAdmin-$latest_version-all-languages.zip"
-    mw phpMyAdmin-$latest_version-all-languages phpmyadmin
-    mw /usr/share/phpmyadmin /usr/share/phpmyadminold
-    mv phpmyadmin /usr/share/phpmyadmin
-    echo "Download and extraction complete."
-else
-    echo "Failed to retrieve the latest version of phpMyAdmin."
-fi
+  #  echo "Downloading phpMyAdmin $latest_version..."
+  #  wget "$download_url"
+  #  
+  #  # Unzip the downloaded file
+  #  unzip "phpMyAdmin-$latest_version-all-languages.zip"
+  #  mw phpMyAdmin-$latest_version-all-languages phpmyadmin
+  #  mw /usr/share/phpmyadmin /usr/share/phpmyadminold
+ #   mv phpmyadmin /usr/share/phpmyadmin
+#    echo "Download and extraction complete."
+#else
+#    echo "Failed to retrieve the latest version of phpMyAdmin."
+#fi
 
 
